1dbeb6fce6c4590abb2ce8ddd8758429fc41a823
Nick Anthony <nanthony007@gmail.com>
2020-07-09 00:17:07 -0400

    Update events module using attrs with validators and converter.

--- events.py
+++ events.py
@@ -1,22 +1,29 @@
 # module for events classes
 
-
 import attr
 from attr.validators import in_, instance_of
 
-
-def make_capital(x):
-    return f"{' '.join(list(map(str.capitalize, x.split())))}"
+import warnings
 
 
-@attr.s(kw_only=True, auto_attribs=True, slots=True)
+def convert_event_name(x: str):
+    """Acts as converter."""
+    if len(x) == 0:
+        warnings.warn(f'String cannot be empty, set to "Generic Event".',
+                      UserWarning)
+        return 'Generic Event'
+    return x.title().strip()
+
+
+@attr.s(kw_only=True, auto_attribs=True, order=False, slots=True, frozen=True)
 class Event(object):
-    id: str = attr.ib(validator=instance_of(str))
-    name: str = attr.ib(converter=make_capital, validator=instance_of(str))
-    type: str = attr.ib(validator=[instance_of(str), in_(("Tournament", "Dual"))])
+    event_id: str = attr.ib(validator=instance_of(str), repr=False)
+    event_name: str = attr.ib(converter=convert_event_name, validator=instance_of(str))
+    event_type: str = attr.ib(validator=[instance_of(str), in_(("Tournament",
+                                                                "Dual Meet"))])
 
-    # can make more strict if want
-    @id.validator
-    def check_id(self, _, value):
-        if len(value) < 10:
-            raise ValueError("id value must be larger than 10")
+    @event_id.validator
+    def check_event_id(self, attrib, val):
+        if len(val) < 20 or len(val) > 50:
+            raise ValueError(f'Expected str `event_id` with 20 <= len <= 50, '
+                             f'got "{val}"')
