3bf6a103d4db9210c48607f11cc299abe6b5ac22
Nick Anthony <nanthony007@gmail.com>
2020-07-21 20:58:14 -0400

    Implement base.Mark class into Event._type attribute.

--- wrestling/events.py
+++ wrestling/events.py
@@ -1,37 +1,49 @@
 # module for events classes
 
 import attr
-from attr.validators import in_, instance_of
-
-from datetime import date
+from attr.validators import instance_of
+import logging
+from wrestling import base
 import warnings
+
+logging.basicConfig(filename='logfile.log')
+logger = logging.getLogger('Event')
 
 
 def convert_event_name(x: str):
     """Acts as converter."""
     if len(x) == 0:
-        warnings.warn(f'String cannot be empty, set to "Generic Event".', UserWarning)
+        message = f'String cannot be empty, set to "Generic Event".'
+        warnings.warn(message, UserWarning)
         return "Generic Event"
     return x.title().strip()
 
 
-@attr.s(kw_only=True, auto_attribs=True, order=False, slots=True, frozen=True)
+@attr.s(auto_attribs=True, order=False, eq=True, slots=True)
 class Event(object):
-    id_: str = attr.ib(validator=instance_of(str), repr=False)
+    id: str = attr.ib(validator=instance_of(str), repr=False)
     name: str = attr.ib(converter=convert_event_name, validator=instance_of(str))
-    type_: str = attr.ib(
-        validator=[instance_of(str), in_(("Tournament", "Dual Meet"))]
+    _type: base.Mark = attr.ib(
+        validator=instance_of(base.Mark), repr=lambda x: x.tag, eq=False
     )
 
-    @id_.validator
-    def _check_event_id(self, _, val):
-        if len(val) < 20 or len(val) > 50:
-            raise ValueError(
-                f"Expected str `id_` with 20 <= len <= 50, " f'got "{val}"'
-            )
+    def __attrs_post_init__(self):
+        self.check_type_input()
+
+    @property
+    def type(self):
+        return self._type.tag
+
+    def check_type_input(self):
+        if self._type.tag not in {"Tournament", "Dual Meet"}:
+            message = f'Invalid Event type, expected one of "Tournament", ' \
+                      f'"Dual Meet", got {self._type.tag!r}.'
+            self._type.isvalid = False
+            self._type.msg = message
+            logger.info(message)
 
     def to_dict(self):
         return dict(
             name=self.name,
-            type=self.type_,
+            type=self.type,
         )
