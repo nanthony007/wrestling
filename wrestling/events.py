c2b49e16f94b2350f54592034df3b6ffb39cdffd
Nick Anthony <nanthony007@gmail.com>
2020-07-23 11:27:52 -0400

    Minor adjustments in syntax, remove id param as unneeded.

--- wrestling/events.py
+++ wrestling/events.py
@@ -21,29 +21,28 @@ def convert_event_name(x: str):
 
 @attr.s(auto_attribs=True, order=False, eq=True, slots=True)
 class Event(object):
-    id: str = attr.ib(validator=instance_of(str), repr=False)
     name: str = attr.ib(converter=convert_event_name, validator=instance_of(str))
-    _type: base.Mark = attr.ib(
-        validator=instance_of(base.Mark), repr=lambda x: x.tag, eq=False
+    _kind: base.Mark = attr.ib(
+        validator=instance_of(base.Mark), repr=lambda x: x.tag, eq=False,
     )
 
     def __attrs_post_init__(self):
         self.check_type_input()
 
     @property
-    def type(self):
-        return self._type.tag
+    def kind(self):
+        return self._kind.tag
 
     def check_type_input(self):
-        if self._type.tag not in {"Tournament", "Dual Meet"}:
+        if self._kind.tag not in {"Tournament", "Dual Meet"}:
             message = f'Invalid Event type, expected one of "Tournament", ' \
-                      f'"Dual Meet", got {self._type.tag!r}.'
-            self._type.isvalid = False
-            self._type.msg = message
+                      f'"Dual Meet", got {self._kind.tag!r}.'
+            self._kind.isvalid = False
+            self._kind.msg = message
             logger.info(message)
 
     def to_dict(self):
         return dict(
             name=self.name,
-            type=self.type,
+            kind=self.kind,
         )
