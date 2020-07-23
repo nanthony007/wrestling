9f4c66c7dff5b45b4d4efa212858953e2f00c46b
Nick Anthony <nanthony007@gmail.com>
2020-07-23 10:24:35 -0400

    Finalize switch to Mark as base class.

--- wrestling/matches.py
+++ wrestling/matches.py
@@ -13,17 +13,12 @@ from urllib.parse import quote
 from wrestling.sequence import isvalid_sequence
 from wrestling import base
 from wrestling.events import Event
-from wrestling.scoring import CollegeScoring, HighSchoolScoring
+from wrestling.scoring import CollegeScoring, HSScoring
 from wrestling.wrestlers import Wrestler
 import logging
 
-logging.basicConfig(filename='logfile.log', filemode='a')
-logger = logging.getLogger('Match')
-
-
-# converts to sorted set
-def convert_ts(ts):
-    return tuple(sorted(ts))
+logging.basicConfig(filename="logfile.log", filemode="a")
+logger = logging.getLogger("Match")
 
 
 @attr.s(frozen=True, slots=True, order=True, eq=True, kw_only=True, auto_attribs=True)
@@ -36,17 +31,19 @@ class Match(object):
     event: Event = attr.ib(
         validator=instance_of(Event), repr=lambda x: x.name, order=False
     )
-    date_: datetime = attr.ib(validator=instance_of(datetime), order=True,
-                              repr=False)
-    result: base.Result = attr.ib(validator=instance_of(base.Result), order=False,
-                                  repr=lambda
-                                      x: x.text)
-    overtime: Optional[bool] = attr.ib(validator=instance_of(bool), order=False,
-                                       repr=False, default=False)
-    focus: Wrestler = attr.ib(validator=instance_of(Wrestler), order=False,
-                              repr=lambda x: x.name)
-    opponent: Wrestler = attr.ib(validator=instance_of(Wrestler), order=False,
-                                 repr=lambda x: x.name)
+    date_: datetime = attr.ib(validator=instance_of(datetime), order=True, repr=False)
+    result: base.Result = attr.ib(
+        validator=instance_of(base.Result), order=False, repr=lambda x: x.text
+    )
+    overtime: Optional[bool] = attr.ib(
+        validator=instance_of(bool), order=False, repr=False, default=False
+    )
+    focus: Wrestler = attr.ib(
+        validator=instance_of(Wrestler), order=False, repr=lambda x: x.name
+    )
+    opponent: Wrestler = attr.ib(
+        validator=instance_of(Wrestler), order=False, repr=lambda x: x.name
+    )
     _weight: base.Mark = attr.ib(validator=instance_of(base.Mark))
 
     def __attrs_post_init__(self):
@@ -72,8 +69,10 @@ class Match(object):
 
     def check_weight_input(self):
         if not self._weight.tag.isdigit():
-            message = f'Invalid weight value, expected a number, ' \
-                      f'got {self._weight.tag!r}.'
+            message = (
+                f"Invalid weight value, expected a number, "
+                f"got {self._weight.tag!r}."
+            )
             self._weight.isvalid = False
             self._weight.msg = message
             logger.info(message)
@@ -104,51 +103,55 @@ class Match(object):
         return sum(
             (
                 event.label.value
-                for event in getattr(self, 'time_series')
+                for event in getattr(self, "time_series")
                 if event.formatted_label.startswith(athlete_filter)
             )
         )
 
     # custom settings for TS bc we need to insert the names and the event name
-    def to_dict(self, ts_only: Optional[bool] = False,
-                results_only: Optional[bool] = False):
+    def to_dict(
+            self, ts_only: Optional[bool] = False, results_only: Optional[bool] = False
+    ):
         if ts_only:
             ts = tuple(
                 dict(
-                    x.to_dict(), **dict(
-                        focus_name=getattr(self, 'focus').name,
-                        opp_name=getattr(self, 'opponent').name,
-                        event_name=getattr(self, 'event').name)
-                ) for x in getattr(self, 'time_series')
+                    x.to_dict(),
+                    **dict(
+                        focus_name=getattr(self, "focus").name,
+                        opp_name=getattr(self, "opponent").name,
+                        event_name=getattr(self, "event").name,
+                    ),
+                )
+                for x in getattr(self, "time_series")
             )
             return ts
         elif results_only:
-            result = getattr(self, 'result').text
+            result = getattr(self, "result").text
             binary, method = result.split()
             return dict(binary=binary, method=method)
         else:
             return dict(
-                focus_name=getattr(self, 'focus').name,
-                focus_team=getattr(self, 'focus').team,
-                opp_name=getattr(self, 'opponent').name,
-                opp_team=getattr(self, 'opponent').team,
-                weight=getattr(self, 'weight_class'),
-                event_name=getattr(self, 'event').name,
-                event_type=getattr(self, 'event').type_,
-                date=datetime.strftime(getattr(self, 'date_'), "%Y-%m-%d %H:%M:%S"),
-                text_result=getattr(self, 'result').text,
-                num_result=getattr(self, 'result').value,
-                duration=getattr(self, 'duration'),
-                overtime=getattr(self, 'overtime'),
-                video=getattr(self, 'video_url'),
-                win=getattr(self, 'result').win,
-                bonus=getattr(self, 'result').bonus,
-                pin=getattr(self, 'result').pin,
-                team_pts=getattr(self, 'result').team_points,
-                focus_pts=getattr(self, 'focus_pts'),
-                opp_pts=getattr(self, 'opp_pts'),
-                mov=getattr(self, 'mov'),
-                td_diff=getattr(self, 'td_diff'),
+                focus_name=getattr(self, "focus").name,
+                focus_team=getattr(self, "focus").team,
+                opp_name=getattr(self, "opponent").name,
+                opp_team=getattr(self, "opponent").team,
+                weight=getattr(self, "weight_class"),
+                event_name=getattr(self, "event").name,
+                event_type=getattr(self, "event").type_,
+                date=datetime.strftime(getattr(self, "date_"), "%Y-%m-%d %H:%M:%S"),
+                text_result=getattr(self, "result").text,
+                num_result=getattr(self, "result").value,
+                duration=getattr(self, "duration"),
+                overtime=getattr(self, "overtime"),
+                video=getattr(self, "video_url"),
+                win=getattr(self, "result").win,
+                bonus=getattr(self, "result").bonus,
+                pin=getattr(self, "result").pin,
+                team_pts=getattr(self, "result").team_points,
+                focus_pts=getattr(self, "focus_pts"),
+                opp_pts=getattr(self, "opp_pts"),
+                mov=getattr(self, "mov"),
+                td_diff=getattr(self, "td_diff"),
             )
 
 
@@ -157,10 +160,9 @@ class CollegeMatch(Match):
     # seconds
     duration: Optional[int] = attr.ib(default=420, validator=instance_of(int))
     # auto sorts (based on time)
-    time_series: Tuple[CollegeScoring] = attr.ib(validator=instance_of(Tuple),
-                                                 order=False,
-                                                 repr=lambda x: f"{len(x)}events"
-                                                 )
+    time_series: Tuple[CollegeScoring] = attr.ib(
+        validator=instance_of(Tuple), order=False, repr=lambda x: f"{len(x)}events"
+    )
 
     def __attrs_post_init__(self):
         Match.__attrs_post_init__(self)
@@ -168,30 +170,34 @@ class CollegeMatch(Match):
     @time_series.validator
     def check_time_series(self, attribute, value):
         if not all(isinstance(event, CollegeScoring) for event in value):
-            raise TypeError(f"All of the items in the `time_series` set must be "
-                            f"`CollegeScoring` objects.")
+            raise TypeError(
+                f"All of the items in the `time_series` set must be "
+                f"`CollegeScoring` objects."
+            )
         # todo: check this function
         # if not isvalid_sequence("college", value):
         #     raise ValueError(f"Time series sequence appears invalid...")
 
 
 @attr.s(frozen=True, slots=True, order=True, eq=True, kw_only=True, auto_attribs=True)
-class HighSchoolMatch(Match):
+class HSMatch(Match):
     # seconds
     duration: Optional[int] = attr.ib(default=360, validator=instance_of(int))
     # auto sorts (based on time)
-    time_series: Tuple[CollegeScoring] = attr.ib(order=False,
-                                                 repr=lambda x: f"{len(x)}events"
-                                                 )
+    time_series: Tuple[HSScoring] = attr.ib(
+        order=False, repr=lambda x: f"{len(x)}events"
+    )
 
     def __attrs_post_init__(self):
         Match.__attrs_post_init__(self)
 
     @time_series.validator
     def check_time_series(self, attribute, value):
-        if not all(isinstance(event, HighSchoolScoring) for event in value):
-            raise TypeError(f"All of the items in the `time_series` set must be "
-                            f"`HighSchoolScoring` objects.")
+        if not all(isinstance(event, HSScoring) for event in value):
+            raise TypeError(
+                f"All of the items in the `time_series` set must be "
+                f"`HighSchoolScoring` objects."
+            )
         # todo: check this function
         # if not isvalid_sequence("college", value):
         #     raise ValueError(f"Time series sequence appears invalid...")
