2ad98e21f111134fda0afc596185595c39774d99
Nick <nanthony007@gmail.com>
2020-07-27 14:20:09 -0400

    Update setup.py.

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-requirements = ["aenum", "attrs", "wheel"]
+requirements = ["attrs", ]
 
 setup(
     name="wrestling",
-    version="0.0.b5",
+    version="0.1",
     author="Nicholas Anthony",
     author_email="nanthony007@gmail.com",
-    description="A package for wrestling statistics and visualizations.",
+    description="A package for wrestling statistics.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nanthony007/wrestling/",
     packages=find_packages(),
     install_requires=requirements,
-    # classifiers=[],
+    # classifiers=[],  # add later
 )
