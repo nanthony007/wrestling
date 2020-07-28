f633301726b74a5fc644e06c89cbf9a745f9a8fc
Nick Anthony <nanthony007@gmail.com>
2020-07-27 22:02:03 -0400

    Documented and formatted/linted.

--- wrestling/events.py
+++ wrestling/events.py
@@ -1,45 +1,84 @@
-# module for events classes
+#!/usr/bin/python
+
+
+"""Module for Events.
+
+This module builds the Events class with validation for its fields. When
+validation is stronger than simple type validation, the Mark class is used
+in replace of traditional str or int classes to track accuracy.
+
+Example:
+    event = Event(name='Practice', kind='Dual Meet')
+
+"""
+
+from typing import Dict
 
 import attr
 from attr.validators import instance_of
+
 from wrestling import base
-import warnings
 
 
-def convert_event_name(x: str):
-    """Acts as converter."""
-    if len(x) == 0:
-        message = f'String cannot be empty, set to "Generic Event".'
-        warnings.warn(message, UserWarning)
-        logger.info(message)
+def convert_event_name(name: str) -> str:
+    """Strips and capitalizes a string.
+
+    This function takes a string input and, if the string length is larger than 1,
+    capitalized the string and strips leading/trailing whitespaces.
+
+    Args:
+        name (str): Any string of any length.
+
+    Returns:
+        str: Capitalized and stripped string.
+    """
+    if len(name) == 0:
         return "Generic Event"
-    return x.title().strip()
+    return name.title().strip()
 
 
 @attr.s(auto_attribs=True, order=False, eq=True, slots=True)
 class Event(object):
+    """Class for storing event related data.
+
+    Args:
+        name (str): Name of the event.
+        kind (Mark): Type of event, either 'Dual Meet' or 'Tournament'.
+
+    """
+
     name: str = attr.ib(converter=convert_event_name, validator=instance_of(str))
     _kind: base.Mark = attr.ib(
         validator=instance_of(base.Mark), repr=lambda x: x.tag, eq=False,
     )
 
     def __attrs_post_init__(self):
+        """Post init function to call Mark input handlers."""
         self.type_input_handler()
 
     @property
-    def kind(self):
-        return self._kind.tag
+    def kind(self) -> str:
+        """Type of event.
+
+        Returns:
+            str: Type of event.
+        """
+        return str(self._kind.tag)
 
     def type_input_handler(self):
+        """Function to manage validity of 'kind' input attribute via Mark class."""
         if self._kind.tag not in {"Tournament", "Dual Meet"}:
-            message = f'Invalid Event type, expected one of "Tournament", ' \
-                      f'"Dual Meet", got {self._kind.tag!r}.'
+            message = (
+                f'Invalid Event type, expected one of "Tournament", '
+                f'"Dual Meet", got {self._kind.tag!r}.'
+            )
             self._kind.isvalid = False
             self._kind.msg = message
-            logger.info(message)
 
-    def to_dict(self):
-        return dict(
-            name=self.name,
-            kind=self.kind,
-        )
+    def to_dict(self) -> Dict:
+        """Creates a dictionary representation of an Event instance.
+
+        Returns:
+            Dict: Dictionary with the name and kind of the Event instance.
+        """
+        return dict(name=self.name, kind=self.kind, )
