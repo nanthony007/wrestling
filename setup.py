db48021b46336d16ded4e6d1699b460620074b34
Nick <nanthony007@gmail.com>
2020-07-29 16:17:53 -0400

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = ["attrs", ]
 
 setup(
     name="wrestling",
-    version="0.2.2",
+    version="0.2.3",
     author="Nicholas Anthony",
     author_email="nanthony007@gmail.com",
     description="A package for wrestling statistics.",
