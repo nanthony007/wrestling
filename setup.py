45680a5f7e3bd13a7abec95c15c7c7e1af4feb50
Nick Anthony <nanthony007@gmail.com>
2021-07-07 17:10:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = ["attrs", ]
 
 setup(
     name="wrestling",
-    version="0.4.5",
+    version="0.4.6",
     author="Nicholas Anthony",
     author_email="nanthony007@gmail.com",
     description="A package for wrestling statistics.",
